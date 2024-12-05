949e9fb1269d24b9004b458bdf5fbeba75271faa
Li Yeo <lifyeo@utu.fi>
2024-12-05 09:42:07 +0200

    Updated dataset and filtering settings

--- forest plot.R
+++ forest plot.R
@@ -1,39 +1,3 @@
-#Alpha forest plot 
-library(ggplot2)
-library(survminer)
-library(survival)
-library(tibble)
-library(mia)
-library(survivalAnalysis)
-#Change into dataframe and format Incident into integer, otherwise error
-a <- as.data.frame(colData(tse))
-a$INCIDENT_HTN <- as.integer(a$INCIDENT_HTN)
-
-#Change the row names
-a <- a %>% dplyr::mutate_at(vars(BMI, BL_AGE, SYSTM), ~./10) %>%
-  dplyr::rename("Age" = BL_AGE ,
-                "Sex"= MEN ,
-                BMI = "BMI",
-                "Diabetes" = PREVAL_DIAB.col_from_pheno,
-                "Systolic blood pressure" = SYSTM,
-                "Diastolic blood pressure" = DIASM,
-                "Healthy Food Choices" = HFC,
-                "Antihypertensive medication" = BP_TREAT ,
-                "Heart failure" = PREVAL_HFAIL_STRICT.col_from_pheno,
-                "Prevalent HTN" = PREVAL_HTN,
-                "Incident HTN" = INCIDENT_HTN,
-                "Hypertension time (yrs)" = HTN_AGEDIFF ,
-                "Smoking" = CURR_SMOKE,
-                "Eastern Finland" = EAST,
-                "Shannon diversity" = shannon)
-
-b<- coxph(Surv(`Hypertension time (yrs)`, `Incident HTN`) ~
-            `Shannon diversity` + Age + Sex + BMI +
-            Diabetes + `Healthy Food Choices` + `Heart failure` + Smoking,
-            data = a)
-ggforest(b) + theme_classic()
-summary(b)
-
 #PERMANOVA table
 rda_info %>%
 knitr::kable() %>%
@@ -47,37 +11,36 @@ CreateTableOne(data = a, strata = "INCIDENT_CKD")
 ##12072024
 #forest plot
 library(readr)
-alpha <- read_csv("coxHTN.csv")
-#alpha <- cox_incidentCKD
-#alpha$'' <- cut(alpha$'P', breaks=c(-Inf, 0.001, 0.01, 0.05, Inf), label=c("***", "**", "*", "")) 
-alpha
-#alpha$Test <- ifelse(is.na(alpha_nephro$Test), "", alpha_nephro$Test)
-alpha$` ` <- paste(rep(" ", 20), collapse = " ")
-
-# Create a confidence interval column to display
-alpha$`HR (95% CI)` <- ifelse(is.na(alpha$se), "",
-                                     sprintf("%.2f (%.2f to %.2f)",
-                                             alpha$`exp(coef)`, alpha$`lower .95`, alpha$`upper .95`))
-alpha
-
 library(grid)
 library(forestploter)
 library(gridExtra)
+alpha <- read_csv("cox-HTN.csv")
+#alpha$'' <- cut(alpha$'P', breaks=c(-Inf, 0.001, 0.01, 0.05, Inf), label=c("***", "**", "*", "")) 
+alpha
+alpha$` ` <- paste(rep(" ", 20), collapse = " ")
+
+# Create a confidence interval column to display
+alpha$`HR (95% CI)` <- ifelse(is.na(alpha$std.error), "",
+                                     sprintf("%.2f (%.2f to %.2f)",
+                                             alpha$estimate, alpha$conf.low, alpha$conf.high))
+alpha
+
 #make the words center
 tm <- forest_theme(base_size = 15,
                    rowhead=list(fg_params=list(hjust=0, x=0)),
                    colhead=list(fg_params=list(hjust=0.5, x=0.5)),
                   #core=list(fg_params=list(hjust=0.5, x=0.5)),
-                   ci_Theight = 0.3)
+                   ci_Theight = 0.3,
+                   ci_lwd = 2)
 
-alpha_ft <- forest(alpha[,c(1, 8, 7, 6)],
-                est = alpha$`exp(coef)`,
-                lower = alpha$`lower .95`, 
-                upper = alpha$`upper .95`,
+alpha_ft <- forest(alpha[,c(2, 10, 9, 6)],
+                est = alpha$estimate,
+                lower = alpha$conf.low, 
+                upper = alpha$conf.high,
                 #sizes = alpha$se,
                 ci_column = 3,
                 ref_line = 1,
-                xlim = c(0, 5),
+                xlim = c(0, 3),
                 x_trans = c("log10"),
                 xlab = "Hazard ratio",
                 theme = tm)
@@ -85,5 +48,8 @@ alpha_ft <- forest(alpha[,c(1, 8, 7, 6)],
 # Print plot
 plot(alpha_ft)
 
-myggsave(alpha)
+# Save plots to PDF, specifying dimensions
+pdf("coxHTN.pdf", width = 9, height = 6)
+print(alpha_ft)
+dev.off()
 
--- table.R
+++ table.R
@@ -6,9 +6,10 @@ sp_tbl <- df_cox_results %>%
   dplyr::filter(stringr::str_detect(term, "taxon")) %>%
   dplyr::mutate(qval_fdr = p.adjust(p.value, method = "BH"))%>%
   dplyr::arrange(qval_fdr) %>%
+  #dplyr::filter(qval_fdr < 0.05) %>%
   dplyr::slice(1:10) %>%
   dplyr::select(taxa,estimate,conf.low, conf.high, p.value, qval_fdr) %>%
-  dplyr::mutate(across(everything(),~ gsub("GUT_","", .))) %>%
+  dplyr::mutate(across(everything(),~ gsub("GUT_Family:","", .))) %>%
   dplyr::mutate_at(c(2:6), as.numeric) %>%
   dplyr::mutate_at(c(2:4),round,2) %>%
   dplyr::mutate_at(c(5:6), round, 3)
@@ -18,7 +19,28 @@ head(sp_tbl)
 
 sp_tbl$`95% CI` <-  sprintf("%s - %s",
                                       sp_tbl$`conf.low`, sp_tbl$`conf.high`)
-write.table(sp_tbl, file = "taxa-coxHTN-full.csv", sep=",", row.names=FALSE)
+write.table(sp_tbl, file = "taxa-coxHTN-mini-G.csv", sep=",", row.names=FALSE)
 
-write.csv(sp_tbl$"../HypertensionMicrobiome/taxa-coxHTN-mini.csv", sp_tbl$tbl)
+#write.csv(sp_tbl$"../HypertensionMicrobiome/taxa-coxHTN-mini.csv", sp_tbl$tbl)
 sp_tbl
+
+#Function table
+tbl <- try%>% 
+  dplyr::filter(!is.na(results)) %>%
+  dplyr::mutate(results = purrr::map(results, ~broom::tidy(.x, exponentiate = TRUE, conf.int = TRUE)))  %>%
+  tidyr::unnest(results) %>%
+  dplyr::filter(term == "term") %>%
+  dplyr::mutate(qval_fdr = p.adjust(p.value, method = "BH")) %>%
+  dplyr::arrange(p.value) %>%
+  dplyr::mutate_if(is.numeric, ~round(.x, digits = 3)) %>%
+  dplyr::slice(1:10) %>%
+  dplyr::select(Pathway,estimate,conf.low, conf.high, p.value, qval_fdr) %>%
+  dplyr::mutate_at(c(2:6), as.numeric) %>%
+  dplyr::mutate_at(c(2:4),round,2) %>%
+  dplyr::mutate_at(c(5:6), round, 2)
+head(tbl)
+
+tbl$`95% CI` <-  sprintf("%s - %s",
+                            tbl$`conf.low`, tbl$`conf.high`)
+write.table(tbl, file = "pathway-rank-mini.csv", sep=",", row.names=FALSE)
+  
